c7b563b34e5d7d545933b3bc139ba304b46f17cf
Deniz Acay <me@denizacay.com>
2025-04-09 21:30:52 +0300

    Polindrom sayı  bulundu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome( -1221));
+        System.out.println(isPalindrome( 707));
+        System.out.println(isPalindrome( 11212));
+
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
+        String reversed = "";// 1=>12 =>121
+        for (int i = digits.length - 1; i >= 0; i--) {
+            reversed += digits[i];
+        }
+        return reversed.equalsIgnoreCase(String.valueOf(number));
     }
 }
